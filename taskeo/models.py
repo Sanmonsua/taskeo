cb980fc09db0d1812dd267e07bd0f97eb4491e77
Sanmonsua <sanmonsua@gmail.com>
2020-07-28 12:02:54 -0500

    Add : category and task models to taskeo app

--- taskeo/models.py
+++ taskeo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=64)
+
+    TO_DO = "To-do"
+    DONE = "Done"
+    STATUS = [
+        ("TO-DO", TO_DO),
+        ("DONE", DONE)
+    ]
+    status = models.CharField(max_length=64, choices=STATUS, default=TO_DO)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    due_date = models.DateTimeField(blank=True)
